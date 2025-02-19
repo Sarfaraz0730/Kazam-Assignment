d5a3d57b241d339d4901faf345d38a20b6c170aa
Sarfaraz0730 <sarfarazpycoder@gmail.com>
2025-02-19 21:24:46 +0530

    home route

--- server/src/routes/Home-route/home.js
+++ server/src/routes/Home-route/home.js
@@ -1,9 +1,8 @@
 const express = require('express');
-const verify = require('../../helper/verify');
 const router = express.Router();
 
 
-router.get("/", verify, async (req, res) => {
+router.get("/", async (req, res) => {
     res.send("Home page");
 });
 
--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -1,10 +1,13 @@
 const express = require('express')
 const Registration = require("./User-routes/Registration")
-
+const home = require("./Home-route/home.js")
 const Login = require("./User-routes/Login")
+const { send } = require('process')
 const router = express.Router()
+router.use("/", home)
 console.log("test")
 router.use("/Registration", Registration)
 router.use("/login", Login)
+// router.get("/", (res,req)=>{res.statusCode(200).send("server is working")})
 
 module.exports = router
